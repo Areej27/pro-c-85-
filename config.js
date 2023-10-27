b15d9368a806bb2486a0043983b9366ea8a2a83d
Areej27 <114870680+Areej27@users.noreply.github.com>
2023-10-27 15:16:12 +0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBvu9IH6yQUbbDXysvUDW1bOxBRLblsS2w",
+    authDomain: "spectagram-app-4b523.firebaseapp.com",
+    projectId: "spectagram-app-4b523",
+    storageBucket: "spectagram-app-4b523.appspot.com",
+    messagingSenderId: "222595246494",
+    appId: "1:222595246494:web:382e614623230400810e8f"
+  };
